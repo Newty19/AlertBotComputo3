fe57ad719ae843f728e0baefc0080119e2107258
Eduardo Rodríguez <a334144@uach.mx>
2023-11-29 01:21:49 -0600

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+config = {
+    "google_api_key": "AIzaSyCzrV_s_ulxxVGbbx5e0kiY2dC8p0vtTXE",
+    "youtube_playlist_id":"PLFdEPVrswDcO_RFISsLrQ92yiAiVO8_r4",
+    "kafka": {
+        "bootstrap.servers": "pkc-4r087.us-west2.gcp.confluent.cloud:9092",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "QWLPMAI4KBYSQHTY",
+        "sasl.password": "k07A/eTahNOST4DMq4SmyIHq7JqY0CmrmOFpSSI+T42bFM6o/T/SOZ0YLxDtiYhm",
+    },
+    "schema_registry": {
+        "url": "https://psrc-4rw99.us-central1.gcp.confluent.cloud",
+        "basic.auth.user.info": "4NLD2TUNE3VVIXCQ:EsivxtznBhuDP8NAAOyX1ac38/zaLOmkWFFY+R44Syb+TLG+Sg/hi7NwrIG8gw19",
+    }
+}
